585f7c1cdd66ce1eed27d9909ef9ffa0b020c462
Jake Litwicki <jake.litwicki@gmail.com>
2018-02-01 08:40:25 -0800

    update deployer

--- s3/tavro-deploy/index.js
+++ s3/tavro-deploy/index.js
@@ -1,32 +1,31 @@
 'use strict';
 
-console.log('Loading function');
-
 const aws = require('aws-sdk');
 const promise = require('bluebird');
-const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 const mailgun = require('mailgun.js');
+
 const ssm = promise.promisifyAll(new aws.SSM());
+const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
-var mgParams = ssm.getParametersAsync({
-    Names: ["MAILGUN_API_KEY", "MAILGUN_PUBLIC_KEY"],
-    WithDecryption: true
-});
+// var mgParams = ssm.getParametersAsync({
+//     Names: ["MAILGUN_API_KEY", "MAILGUN_PUBLIC_KEY"],
+//     WithDecryption: true
+// });
 
-console.log(mgParams);
+// console.log(mgParams);
 
 var params = {};
-for (p of mgParams.Parameters) {
-    params[p.Name] = p.Value;
-}
+// for (p of mgParams.Parameters) {
+//     params[p.Name] = p.Value;
+// }
 
-console.log(params);
+// console.log(params);
 
 //var mailer = mailgun.client({username: 'api', key: params['MAILGUN_API_KEY']});
 var mailer = mailgun.client({
     username: 'api',
-    key: params['MAILGUN_API_KEY'],
-    public_key: params['MAILGUN_PUBLIC_KEY']
+    key: params['MAILGUN_API_KEY'] || 'key-',
+    public_key: params['MAILGUN_PUBLIC_KEY'] || 'pubkey-'
 });
 
 exports.handler = (event, context, callback) => {
@@ -40,31 +39,35 @@ exports.handler = (event, context, callback) => {
         Bucket: bucket,
         Key: key,
     };
+
+    var url = "http://" + Bucket + ".s3.amazonaws.com/" + key;
+    sendmail();
     
-    s3.getObject(params, (err, data) => {
-        
-        console.log(data);
+    // s3.getObject(params, (err, data) => {
+    //     console.log(data);
+    //     if (err) {
+    //         console.log(err);
+    //         const message = "Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.";
+    //         console.log(message);
+    //         callback(message);
+    //     } else {
+    //         
+    //     }
 
-        if (err) {
-            console.log(err);
-            const message = "Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.";
-            console.log(message);
-            callback(message);
-        } else {
+    // });
 
-            var url = "http://" + Bucket + ".s3.amazonaws.com/" + key;
+    function sendmail(url, callback) {
 
-            mg.messages.create('dev@zoadilack.com', {
-                from: "Zoadilack <dev@zoadilack.com>",
-                to: ["dev@zoadilack.com"],
-                subject: "New Tavro Build: " + key,
-                text: "A new Tavro build is complete: " + url,
-                html: "<h1>New Tavro Build</h1><p>A new Tavro build is complete: " + url + "</p>"
-            })
-                .then(msg => console.log(msg)) // logs response data
-                .catch(err => console.log(err)); // logs any error
+        mg.messages.create('dev@zoadilack.com', {
+            from: "Zoadilack <dev@zoadilack.com>",
+            to: ["dev@zoadilack.com"],
+            subject: "New Tavro Build: " + key,
+            text: "A new Tavro build is complete: " + url,
+            html: "<h1>New Tavro Build</h1><p>A new Tavro build is complete: " + url + "</p>"
+        })
+        .then(msg => console.log(msg)) // logs response data
+        .catch(err => console.log(err)); // logs any error
 
-        }
+    }
 
-    });
 };
